19a76c42517cd69e4f824bc1e21e7ad63bc34283
Johnpope7 <Johnpope5387@gmail.com>
2022-11-09 21:42:45 -0700

    Organized Prefab Components
    Organized prefab components and created list generators for each classification of component

---
+++ Assets/Pawns/Pawn Scripts/Frame Scripts/PawnFrame.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PawnFrame : MonoBehaviour
+{
+    #region Variables
+    [SerializeField]
+    GameObject skeleton;
+    [SerializeField]
+    GameObject head;
+    [SerializeField]
+    GameObject body;
+    [SerializeField]
+    GameObject leftArm;
+    [SerializeField]
+    GameObject rightArm;
+    [SerializeField]
+    GameObject legs;
+    #endregion
+
+    #region Functions
+    #region Builtin Functions
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    #endregion
+    #region Custom Functions
+    #endregion
+    #endregion
+}
---
+++ Assets/Pawns/Pawn Scripts/Generation Scripts/Assembler.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Assembler : MonoBehaviour
+{
+    #region Variables
+    [Header("Component Transforms")]
+    Transform transformHeadpoint;
+    Transform transformBodypoint;
+    Transform transformLeftArmpoint;
+    Transform transformRightArmpoint;
+    Transform transformLegpoint;
+
+    [Header("All Components List")]
+    public List<GameObject> allPartsList;
+    public GameObject[] allPartsArray;
+    [Header("Head Components List")]
+    public List<GameObject> headList;
+    public GameObject[] headArray;
+    [Header("Body Components List")]
+    public List<GameObject> bodyList;
+    public GameObject[] bodyArray;
+    [Header("Left Arm Components List")]
+    public List<GameObject> leftArmList;
+    public GameObject[] leftArmArray;
+    [Header("Right Arm Components List")]
+    public List<GameObject> rightArmList;
+    public GameObject[] rightArmArray;
+    [Header("Leg Components List")]
+    public List<GameObject> legList;
+    public GameObject[] legArray;
+    #endregion
+
+    #region Functions
+    public void AllPartsListLoader() 
+    {
+        allPartsArray = Resources.LoadAll<GameObject>("Pawn Pieces");
+        allPartsList = allPartsArray.ToList();
+    }
+    public void HeadListLoader()
+    {
+        headArray = Resources.LoadAll<GameObject>("Heads");
+        headList = headArray.ToList();
+    }
+    public void BodyListLoader()
+    {
+        bodyArray = Resources.LoadAll<GameObject>("Pawn Pieces");
+        bodyList = allPartsArray.ToList();
+    }
+    public void LeftArmListLoader()
+    {
+        rightArmArray = Resources.LoadAll<GameObject>("Pawn Pieces");
+        rightArmList = allPartsArray.ToList();
+    }
+    public void RightArmListLoader()
+    {
+        allPartsArray = Resources.LoadAll<GameObject>("Pawn Pieces");
+        allPartsList = allPartsArray.ToList();
+    }
+    public void LegsListLoader()
+    {
+        legArray = Resources.LoadAll<GameObject>("Pawn Pieces");
+        legList = allPartsArray.ToList();
+    }
+    public GameObject Assemble() 
+    { 
+     //new GameObject Humanoid = Instantiate()
+    }
+    #endregion
+}
